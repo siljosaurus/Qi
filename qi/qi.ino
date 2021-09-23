93f05cb39c9cd5ef5e3bf1de65a71d04e6320017
Claudia Sikora <Cloudy@eduroam-193-157-247-205.wlan.uio.no>
2021-09-23 11:55:15 +0200

    added pressure sensor, solcelle and battery up/down code

--- qi/qi.ino
+++ qi/qi.ino
@@ -6,23 +6,34 @@
 
 int batteri = 0; // dette er en skala hvor 0 er shutdown, og 9 er fullt.
 
-int slange_pin = A0;
-int leds_i_slange = 5;
-Adafruit_NeoPixel slange = Adafruit_NeoPixel(leds_i_slange, slange_pin,NEO_GRB + NEO_KHZ800);
+//int slange_pin = A0;
+
+int leds_i_solcelleslange = 60;
+int aktive_leds_i_solcelleslange = 0;
+
+int pressure_pin = A1;
+int pressure_reading; //variable for storing our reading
+int photocellPin = A0;
+int photocellReading;
+int LEDpin = 11;
+int LEDbrightness;
+//Adafruit_NeoPixel slange = Adafruit_NeoPixel(leds_i_slange, slange_pin,NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel solcelle_slange = Adafruit_NeoPixel(leds_i_solcelleslange, LEDpin,NEO_GRB + NEO_KHZ800);
 CapacitiveSensor touch = CapacitiveSensor(4,2); // 1M resistor between pins 4 & 2, pin 2 is sensor pin
 
 
 void setup() {
-  slange.begin();
-  slange.show();
-  touch.set_CS_AutocaL_Millis(0xFFFFFFFF);
   Serial.begin(9600);
+  //slange.begin();
+  solcelle_slange.begin();
+  touch.set_CS_AutocaL_Millis(0xFFFFFFFF);
+  
 }
 
 
-void loop() {
- 
-  Styr_batteriet();
+void loop(void) {
+  Solcelle();
+  // Styr_batteriet();
   
   //sjekke om noen tar på metal nylon fabric sheet
   //sjekke om noen lyser på designerte plasser
@@ -34,16 +45,15 @@ void loop() {
   //if (lader){
   //batterilevel -1
   //}
-  delay(100);
+  //delay(100);
 }
 
  
 
 void Styr_batteriet(){
-  for (int i= 0; i<leds_i_slange; i++) {
-    slange.setPixelColor(i, 0, 0, 255);
-    slange.show();
-    }
+  //for (int i= 0; i<leds_i_slange; i++) {
+    //slange.setPixelColor(i, 0, 0, 255);
+    //slange.show();}
   
   /*if (batterilevel == 0){
     //ingen lys
@@ -57,9 +67,31 @@ void Styr_batteriet(){
 }
 
 
-void Batteriet_lades_opp() {}
-void Batteriet_lades_ned(){}
-void Batteriet_kollapser(){}
+void Batteriet_lades_opp() {
+  if (batteri < 9) {
+    batteri += 1;
+    }
+    else {
+      Batteri_fullt();
+      }
+    delay(2000); // 2 sek for å lade opp
+  }
+  
+void Batteriet_lades_ned() {
+  if (batteri > 0) {
+    batteri -= 1;
+    }
+    else {
+      Batteri_shutdown();
+      }
+    delay(1000); // 1 sek for å lade ned
+  }
+  
+void Batteriet_kollapser() {
+    //skjer kun ved mobillading
+    //if (batteri == 0) {}
+    //else {Batteriet_lades_ned();}
+  }
 
 void Batteri_fullt(){
   // batteri = 9
@@ -73,13 +105,14 @@ void Batteri_lavt(){
   // batteri = mellom 3 og 1
   }
   
-void Batteri_shutdowm(){
+void Batteri_shutdown(){
   // batteri = 0
   }
 
  
 
 void Gi_varme(){
+  Batteriet_lades_opp();
   //funksjon for å gi varme ved registrering, hvordan peltier skal oppføre seg ved interaksjon
   //< 5 sek: level 1 (kaldest, romtemp)
   //< 10 && > 5 sek: level 2 (litt varmt)
@@ -98,81 +131,76 @@ boolean Registrer_touch(){
 }
 
  
-
-void Photoresistor(){
+unsigned long prev = 0;
+ 
+void Solcelle(){
   
-  photocellReading = analogRead(photocellPin);
+  int photocellReading = analogRead(photocellPin);
   Serial.print("Analog reading = ");
   Serial.println(photocellReading);
 
   //photocellReading = 1023 - photocellReading;
-  LEDbrightness = map(photocellReading, 0, 210, 0, 255);
-  Serial.println(LEDbrightness);
-  analogWrite(LEDpin, LEDbrightness);
-  delay(100);
   
-  //registrer mengde lys som blir sendt inn
- //lengde man gir lys gir batteriet mer/mindre energi
- // < 5 sek: level 1 (kaldest, romtemp)
- // < 10 && > 5 sek: level 2 (litt varmt)
- // < 15 && > 10 sek: level 3 (varmest)
+ // for (int i= 0; i<leds_i_slange; i++) {
+    // solcelle_slange.setPixelColor(i, 0, 0, 255);
+    //solcelle_slange.show();}
+    
+  //LEDbrightness = map(photocellReading, 0, 210, 0, 255);
+  //Serial.println(LEDbrightness);
 
- 
-
- /*
- 
-  while(True){
+    int num_leds_to_show = map(photocellReading, 60, 700, 0, leds_i_solcelleslange);
     //peak = alt lyser
-    diff = peak - num_leds_to_show
+    Serial.println(num_leds_to_show);
+    int diff = num_leds_to_show - aktive_leds_i_solcelleslange;
 
- 
-
-    if (diff > 1){
-      diff = 1; // Hvorfor?
+    if (diff > 1) {
+      unsigned long now = millis();
+      if (now - prev > 100) { //har det gått mer enn et sekund siden forrige økning?:
+         diff = 1; // 
+         prev = now;
+        } else {
+          diff = 0;
+          }
     }
 
- 
-
-    num_leds_to_show = num_leds_to_show + diff;
-
- 
-
+    num_leds_to_show = aktive_leds_i_solcelleslange + diff;
+    aktive_leds_i_solcelleslange = num_leds_to_show;
+    
     if(num_leds_to_show > 40){
-      num_leds_to_show = 59;
+      // batteri_ladesoppeller ned
     }
 
- 
 
-    //light value remapped to pixel position
-    if (light.value < 0){
-      //fyll lyslenken
-    }
 
- 
-
-    for (int i=0; i < maxPixels; i++){
+    for (int i=0; i < leds_i_solcelleslange; i++){
       if (i <= num_leds_to_show){
-        if (i <= 40){
+        Serial.print("!");
+        solcelle_slange.setPixelColor(i, solcelle_slange.Color(0, 150, 0));
+        /*if (i <= 40){
           pixels[i] = (0, 100, 50);
         }
         else{
           pixels[i] = (255, 255, 0);
-        }
+        }*/
       }
        else{
-          pixels[i] = (0, 0, 0);
+        Serial.print(".");
+          solcelle_slange.setPixelColor(i, solcelle_slange.Color(0, 0, 0));
         }
-    }
-     pixels.show()
-    time.sleep(0.5)
     
-  }*/
+     solcelle_slange.show();
+    
+  }
   
 }
 
  
 
 boolean Lad_mobil(){
+  if (pressure_reading > 0) {
+    Batteriet_kollapser();
+    return true;
+    }
   //lader en mobil i et gitt tidsrom, hvis lenger kutt strømmen
 }
 
